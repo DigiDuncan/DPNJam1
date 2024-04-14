d9e0e7655a5db9e5f1ec67f078467de7ff0b05e0
DigiDuncan <digiduncan@gmail.com>
2024-04-14 13:22:41 -0400

    start battling

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -7,14 +7,19 @@ var game_over: bool = false
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 func _on_book_player_win():
 	pass # Replace with function body.
 
 func _on_book_player_lose():
 	pass # Replace with the player being a looser. Boowomp :(
+
+
+func _on_summon_button_pressed():
+	var enemy: Monster = $"../Monsters/EnemyMonster"
+	var ally: Monster = $"../Monsters/AllyMonster"
+
+	print(enemy.defend(ally))
